34ea88a314cee706fba5c4872b5493345ca73746
Armando Serrano <you@example.com>
2019-01-25 23:45:13 -0500

    POST GET

--- rss/app.py
+++ rss/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 import feedparser
 from flask import render_template
+from flask import request
+import json
 
 app = Flask(__name__)
 RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
@@ -27,12 +29,40 @@ RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 # def fox():
 #     return get_news('fox')
 
+
+
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="bbc"):
+#http://127.0.0.1:5000/cnn
+#@app.route("/<publication>")
+#def get_news(publication="bbc"):
+
+#http://127.0.0.1:5000/?publication=cnn
+def get_news():
+    publication=request.args.get("publication")
+    print(publication)
+    if not publication or publication.lower() not in RSS_FEEDS:
+        return 404
+    else :
+        publication=publication.lower()
     feed = feedparser.parse(RSS_FEEDS[publication])
     articulos = feed['entries']
     return render_template('home.html',articles=articulos)
+
+@app.route("/",methods=["POST"])
+def get_news_post():
+    publication=request.get_json()
+    publication=publication['publication']
+    print()
+    if not publication or publication.lower() not in RSS_FEEDS:
+         return 404
+    else :
+         publication=publication.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    articulos = feed['entries']
+    return render_template('home.html',articles=articulos)
+
+
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
